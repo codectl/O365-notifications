dcb1a2fd8c146562419ed8d16dcf22befc759ea2
Renato Teixeira <rena2damas@gmail.com>
2022-07-28 16:28:15 +0100

    Fix imports

--- src/O365_notifications/push/base.py
+++ src/O365_notifications/push/base.py
@@ -1,16 +1,12 @@
-from o365_notifications.base import O365Notification, O365Notifications
+from O365_notifications.base import O365Notification, O365Notifications
 
 
 class O365PushNotification(O365Notification):
-    """O365 Push Notification"""
-
     def __init__(self, parent=None, **kwargs):
         super().__init__(parent=parent, **kwargs)
 
 
 class O365PushNotifications(O365Notifications):
-    """O365 Push Notifications"""
-
     def subscribe(self, *, resource):
         raise NotImplementedError("TODO: must implement this method.")
 
