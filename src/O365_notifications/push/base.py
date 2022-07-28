9c25881741e976916a537272f84116363a255c29
Renato Teixeira <rena2damas@gmail.com>
2022-07-28 22:32:11 +0100

    Remove redundant __init__

--- src/O365_notifications/push/base.py
+++ src/O365_notifications/push/base.py
@@ -2,8 +2,7 @@ from O365_notifications.base import O365Notification, O365Notifications
 
 
 class O365PushNotification(O365Notification):
-    def __init__(self, parent=None, **kwargs):
-        super().__init__(parent=parent, **kwargs)
+    pass
 
 
 class O365PushNotifications(O365Notifications):
