32c671a7ab02f8838db21a2799529463316cdade
Renato Teixeira <rena2damas@gmail.com>
2022-08-07 19:12:08 +0100

    test: additional tests & asserts #wip

--- tests/streaming/test_streaming.py
+++ tests/streaming/test_streaming.py
@@ -1,14 +1,14 @@
-import O365
 import pytest
+from O365 import Account, MSGraphProtocol, MSOffice365Protocol
 
 from O365_notifications.constants import O365EventType
 from O365_notifications.streaming import O365StreamingSubscriber
 
 
-@pytest.fixture(scope="class")
-def account(backend):
-    protocol = O365.MSOffice365Protocol(api_version="beta")
-    return O365.Account(
+@pytest.fixture(scope="class", params=(MSOffice365Protocol, MSGraphProtocol))
+def account(backend, request):
+    protocol = request.param(api_version="beta")
+    return Account(
         credentials=("user", "pass"),
         tenant_id="foo",
         main_resource="foo@bar.com",
@@ -28,24 +28,22 @@ def subscriber(account, folder):
     return O365StreamingSubscriber(parent=account)
 
 
-@pytest.fixture(scope="class")
-def outlook_subscription(account):
-    base_url = "https://outlook.office.com/api/beta/"
-    return {
-        "@odata.context": f"{base_url}/...",
-        "@odata.type": "#Microsoft.OutlookServices.StreamingSubscription",
-        "@odata.id": f"{base_url}/users/foo@bar.com/Subscriptions('RUM4OEJFNUIQUQ4MQ')",
-        "Id": "RUM4OEJFNUIQUQ4MQ",
-        "Resource": f"{base_url}/me/mailfolders('inbox')/Messages",
-        "ChangeType": "Created",
-    }
-
-
 class TestMailbox:
-    def test_subscribe(self, subscriber, folder, outlook_subscription, requests_mock):
-        response = outlook_subscription
-        base_url = f"{subscriber.protocol.service_url}{subscriber.main_resource}"
-        requests_mock.register_uri("POST", f"{base_url}/subscriptions", json=response)
+    def test_subscription(self, subscriber, folder, requests_mock):
+        proto_url = f"{subscriber.protocol.service_url}{subscriber.main_resource}"
+        sub_type = subscriber.namespace.O365SubscriptionType.STREAMING_SUBSCRIPTION
+        response = {
+            "@odata.context": f"{proto_url}/...",
+            "@odata.type": sub_type.value,
+            "@odata.id": f"{proto_url}/users/foo@bar.com/Subscriptions('xyz')",
+            "Id": "xyz",
+            "Resource": f"{proto_url}/me/mailfolders('inbox')/Messages",
+            "ChangeType": "Created",
+        }
+        requests_mock.register_uri("POST", f"{proto_url}/subscriptions", json=response)
         subscriber.subscribe(resource=folder, events=[O365EventType.CREATED])
         assert len(subscriber.subscriptions) == 1
+        assert subscriber.subscriptions[0].type == sub_type
+        assert subscriber.subscriptions[0].id == "xyz"
+        assert subscriber.subscriptions[0].events == [O365EventType.CREATED]
         assert subscriber.subscriptions[0].raw == response
