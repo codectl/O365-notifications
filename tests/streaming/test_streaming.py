a115d7b7dcc13c18bc60fc001c189f1f8348eec8
Renato Teixeira <rena2damas@gmail.com>
2022-08-07 19:16:45 +0100

    Simplify test

--- tests/streaming/test_streaming.py
+++ tests/streaming/test_streaming.py
@@ -28,28 +28,23 @@ def subscriber(account, folder):
     return O365StreamingSubscriber(parent=account)
 
 
-@pytest.fixture(scope="class")
-def outlook_subscription(account):
-    base_url = "https://outlook.office.com/api/beta/"
-    return {
-        "@odata.context": f"{base_url}/...",
-        "@odata.type": "#Microsoft.OutlookServices.StreamingSubscription",
-        "@odata.id": f"{base_url}/users/foo@bar.com/Subscriptions('RUM4OEJFNUIQUQ4MQ')",
-        "Id": "RUM4OEJFNUIQUQ4MQ",
-        "Resource": f"{base_url}/me/mailfolders('inbox')/Messages",
-        "ChangeType": "Created",
-    }
-
-
 class TestMailbox:
-    def test_subscribe(self, subscriber, folder, outlook_subscription, requests_mock):
-        response = outlook_subscription
-        base_url = f"{subscriber.protocol.service_url}{subscriber.main_resource}"
-        requests_mock.register_uri("POST", f"{base_url}/subscriptions", json=response)
+    def test_subscribe(self, subscriber, folder, requests_mock):
+        proto_url = "https://outlook.office.com/api/beta/"
+        response = {
+            "@odata.context": f"{proto_url}/...",
+            "@odata.type": "#Microsoft.OutlookServices.StreamingSubscription",
+            "@odata.id": f"{proto_url}/users/foo@bar.com/Subscriptions('xyz')",
+            "Id": "xyz",
+            "Resource": f"{proto_url}/me/mailfolders('inbox')/Messages",
+            "ChangeType": "Created",
+        }
+        req_url = f"{subscriber.protocol.service_url}{subscriber.main_resource}"
+        requests_mock.register_uri("POST", f"{req_url}/subscriptions", json=response)
         subscriber.subscribe(resource=folder, events=[O365EventType.CREATED])
         sub_type = subscriber.namespace.O365SubscriptionType.STREAMING_SUBSCRIPTION
         assert len(subscriber.subscriptions) == 1
         assert subscriber.subscriptions[0].type == sub_type
-        assert subscriber.subscriptions[0].id == "RUM4OEJFNUIQUQ4MQ"
+        assert subscriber.subscriptions[0].id == "xyz"
         assert subscriber.subscriptions[0].events == [O365EventType.CREATED]
         assert subscriber.subscriptions[0].raw == response
