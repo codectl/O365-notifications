92ee93978e33d96e1525c7c17a4b6a714f78856d
Renato Teixeira <rena2damas@gmail.com>
2022-08-08 21:31:09 +0100

    test: dummy test for streaming connection @wip

--- tests/streaming/test_streaming.py
+++ tests/streaming/test_streaming.py
@@ -36,18 +36,18 @@ def subscriber(account):
 @pytest_cases.parametrize("resource", [fixture_ref("inbox")])
 @pytest_cases.parametrize("events", [[O365EventType.CREATED]])
 def subscribe(resource, events, subscriber, requests_mock):
-    proto_url = f"{subscriber.protocol.service_url}{subscriber.main_resource}"
+    base_url = f"{subscriber.protocol.service_url}{subscriber.main_resource}"
     sub_type = subscriber.namespace.O365SubscriptionType.STREAMING_SUBSCRIPTION
     random_id = str(random.randint(1000, 9999))
     response = {
-        "@odata.context": f"{proto_url}/...",
+        "@odata.context": f"{base_url}/...",
         "@odata.type": sub_type.value,
-        "@odata.id": f"{proto_url}/users/foo@bar.com/Subscriptions('{random_id}')",
+        "@odata.id": f"{base_url}/users/foo@bar.com/Subscriptions('{random_id}')",
         "Id": random_id,
-        "Resource": f"{proto_url}/me/mailfolders('inbox')/Messages",
+        "Resource": f"{base_url}/me/mailfolders('inbox')/Messages",
         "ChangeType": ",".join(e.value for e in events),
     }
-    requests_mock.register_uri("POST", f"{proto_url}/subscriptions", json=response)
+    requests_mock.register_uri("POST", f"{base_url}/subscriptions", json=response)
     subscriber.subscribe(resource=resource, events=events)
 
 
@@ -65,6 +65,11 @@ class TestMailbox:
 
     @pytest_cases.parametrize("subscription", [fixture_ref("subscribe")])
     def test_streaming_connection(self, subscription, subscriber, requests_mock):
-        proto_url = f"{subscriber.protocol.service_url}{subscriber.main_resource}"
-        requests_mock.register_uri("POST", f"{proto_url}/GetNotifications", json={})
+        proto_url = subscriber.protocol.service_url
+        base_url = f"{proto_url}{subscriber.main_resource}"
+        response = {
+            "@odata.context": f"{proto_url}/metadata#Notifications",
+            "value": [],
+        }
+        requests_mock.register_uri("POST", f"{base_url}/GetNotifications", json={})
         subscriber.create_event_channel()
